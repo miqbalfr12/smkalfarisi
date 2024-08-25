d9d434c0814e7f7363ba284e2855d2d364e98d24
miqbalfr12 <miqbalfr12@gmail.com>
2024-08-25 23:32:37 +0700

    fix slider

--- resources/views/galerikegiatan.blade.php
+++ resources/views/galerikegiatan.blade.php
@@ -1,4 +1,8 @@
 <x-layouts.app title="SMK Al-Farisi Leles Garut">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/keen-slider@6.8.5/keen-slider.min.css"
+    />
     <div class="w-full p-4 mx-auto max-w-7xl">
         <x-atoms.cta text="Galeri Kegiatan SMK-Al Farisi" />
 
@@ -7,7 +11,7 @@
             <div class="py-4" id="{{ str_replace(' ', '-', strtolower($gallery['nama'])) }}">
                 <h3 class="mb-2 text-2xl font-semibold">{{ $gallery['nama'] }}</h3>
 
-                <x-organisms.swiper :items="$gallery['items']" :path="$gallery['path']" />
+                <x-organisms.swiper :items="$gallery['items']" :path="$gallery['path']" :index="$loop->index" />
             </div>
         @endforeach
     </div>
